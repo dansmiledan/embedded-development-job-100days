527938d62b9e32b7aac9f63f8351339cf4f9878c
dansmiledan <yntantan@icloud.com>
2023-05-22 02:22:54 +0000

    ioctl test; misc device

---
+++ helloworld/app/ioctl_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int my_dev = open("/dev/mydev", 0);
+    
+    if (my_dev < 0) {
+        perror("Fail to open device file: /dev/mydev.");
+    } else {
+        ioctl(my_dev, 100, 110);
+        close(my_dev);
+    }
+    return 0;
+}
--- helloworld/char_device_driver_basic.c
+++ helloworld/char_device_driver_basic.c
@@ -4,8 +4,16 @@
 #include "linux/export.h"
 #include "linux/fs.h"
 #include "linux/kdev_t.h"
+#include "linux/platform_device.h"
 #include <linux/cdev.h>
 #include <linux/module.h>
+
+#define EEP_MAGIC        'E'
+#define EEP_CMD_0_SEQ_NO 0x1
+#define EEP_CMD_1_SEQ_NO 0x2
+
+#define EEP_CMD_0 _IO(EEP_MAGIC, EEP_CMD_0_SEQ_NO)
+#define EEP_CMD_1 _IOW(EEP_MAGIC, EEP_CMD_1_SEQ_NO, unsigned long)
 
 #define EEP_TEST_BASE   1000
 #define EEP_NBANK       8
@@ -21,15 +29,82 @@ struct class *eep_class;
 struct eep_data eep_data[EEP_NBANK];
 dev_t dev_num;
 
-int
+static int
 my_open(struct inode *inode, struct file *filp) {
     struct eep_data *my_data = container_of(inode->i_cdev, struct eep_data, eep_cdev);
     pr_info("enter %s, get predefined test data %d\n", __FUNCTION__, my_data->test_data);
     return 0;
 }
 
+/*
+ * free memeory
+ * close device; drop buffer;
+ */
+static int
+my_release(struct inode *inode, struct file *filp) {
+    pr_info("enter %s\n", __FUNCTION__);
+    return 0;
+}
+
+/*
+ * 0 means EOF
+ */
+static ssize_t
+my_read(struct file *filp, char __user *buf, size_t count, loff_t *pos) {
+    pr_info("enter %s\n", __FUNCTION__);
+    return 0;
+}
+
+/*
+ * try:
+ * memory size
+ * address translation
+ * data copy
+ *
+ */
+static ssize_t
+my_write(struct file *filp, const char __user *buf, size_t count, loff_t *pos) {
+    pr_info("enter %s\n", __FUNCTION__);
+    return 0;
+}
+
+static loff_t
+my_llseek(struct file *filp, loff_t offset, int whence) {
+    pr_info("enter %s\n", __FUNCTION__);
+    return 0;
+}
+
+static unsigned int
+my_poll(struct file *filp, struct poll_table_struct *pt) {
+    pr_info("enter %s\n", __FUNCTION__);
+    return 0;
+}
+
+static long
+my_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
+    pr_info("enter %s\n", __FUNCTION__);
+    switch (cmd) {
+    case EEP_CMD_0:
+        pr_info("recv cmd 0");
+        break;
+    case EEP_CMD_1:
+        pr_info("recv cmd 1");
+        break;
+    default:
+        return -ENOTTY;
+    }
+    return 0;
+}
+
 const struct file_operations fops = {
+    .owner = THIS_MODULE,
     .open = my_open,
+    .release = my_release,
+    .write = my_write,
+    .read = my_read,
+    .llseek = my_llseek,
+    .poll = my_poll,
+    .compat_ioctl = my_ioctl,
 };
 
 static int __init
